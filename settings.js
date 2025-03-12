734f6d56776ea83efb9f7a6211266656eaddbd94
RayadTech <sauvegardecompteff@gmail.com>
2025-03-12 08:53:44 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "RayadTech Bot",
+  botOwner: 'Rayad-tech', // Your name
+  ownerNumber: '0556246931', // your number 
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
